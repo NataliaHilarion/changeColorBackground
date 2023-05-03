592a807bce6ffe4002c3b51b12b463b114c61d88
Natalia Hilarion <natalia.hilarion@outlook.com>
2023-05-03 09:50:42 -0500

    javascript de color simple

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const d = document
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const $btn = d.getElementById("btn")
+const $color = d.querySelector(".color")
+
+$btn.addEventListener("click", function(){
+  const randomNumber = getRandomNumber()
+  console.log(randomNumber);
+  d.body.style.backgroundColor = colors[randomNumber]
+  $color.textContent = colors[randomNumber]
+})
+
+function getRandomNumber(){
+  return Math.floor(Math.random() * colors.length)
+}
+
